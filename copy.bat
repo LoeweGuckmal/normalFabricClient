eb605d0261f81cf543f4bee5fee753f9b654fbb9
LoeweGuckmal <loegue123123@gmail.com>
2023-08-30 13:26:44 +0200

    upgraded to new System

---
+++ copy.bat
@@ -0,0 +1,2 @@
+xcopy C:\Users\Simon\Documents\Dokumente\Minecraft\Minecraft_Mod\normalFabricClient\src\main C:\Users\Simon\Documents\Dokumente\Minecraft\Minecraft_Mod\GitHub\normal-Mod\src\main /Y/E
+xcopy C:\Users\Simon\Documents\Dokumente\Minecraft\Minecraft_Mod\normalFabricClient\gradle.properties C:\Users\Simon\Documents\Dokumente\Minecraft\Minecraft_Mod\GitHub\normal-Mod\ /Y/E
